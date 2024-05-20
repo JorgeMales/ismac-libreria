ed52fe8ae178a55334fc4dc8362661c7239da6a1
MATUTINO <MATUTINO@DESKTOP-5HH3LJH>
2024-05-20 11:28:21 -0500

    JQv0001 - Este es mi primer cambio en el git

---
+++ src/com/distribuida/entities/FacturaDetalle.java
@@ -0,0 +1,75 @@
+package com.distribuida.entities;
+
+public class FacturaDetalle {
+
+	private int idFacturaDetalle;
+	private int cantidad;
+	private double subTotal;
+	private Factura factura;
+	private Libro libro;
+	//private int idFactura;
+	//private int idLibro;
+	
+	public FacturaDetalle() {}
+
+	public FacturaDetalle(int idFacturaDetalle, int cantidad, double subTotal) {
+	
+		this.idFacturaDetalle = idFacturaDetalle;
+		this.cantidad = cantidad;
+		this.subTotal = subTotal;
+		//this.idFactura = idFactura;
+		//this.idLibro = idLibro;
+	}
+
+	public int getIdFacturaDetalle() {
+		return idFacturaDetalle;
+	}
+
+	public void setIdFacturaDetalle(int idFacturaDetalle) {
+		this.idFacturaDetalle = idFacturaDetalle;
+	}
+
+	public int getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+
+	public double getSubTotal() {
+		return subTotal;
+	}
+
+	public void setSubTotal(double subTotal) {
+		this.subTotal = subTotal;
+	}
+
+
+
+	public Factura getFactura() {
+		return factura;
+	}
+
+	public void setFactura(Factura factura) {
+		this.factura = factura;
+	}
+
+	public Libro getLibro() {
+		return libro;
+	}
+
+	public void setLibro(Libro libro) {
+		this.libro = libro;
+	}
+
+	@Override
+	public String toString() {
+		return "FacturaDetalle [idFacturaDetalle=" + idFacturaDetalle + ", cantidad=" + cantidad + ", subTotal="
+				+ subTotal + ", factura=" + factura + ", libro=" + libro + "]";
+	}
+
+	
+	
+
+}
